5b4a4516e064b334f6b1f425e136af9244abb535
Roman Mineyev <mineyev2@illinois.edu>
2026-05-16 19:21:45 -0400

    added videos to carousel

--- static/js/index.js
+++ static/js/index.js
@@ -14,7 +14,21 @@
       if (rel === 0) slide.classList.add('is-active');
       else if (rel === total - 1) slide.classList.add('is-prev');
       else if (rel === 1) slide.classList.add('is-next');
+
+      const video = slide.querySelector('video');
+      if (video) {
+        if (rel === 0) {
+          video.currentTime = 0;
+          video.play().catch(() => {});
+        } else {
+          video.pause();
+          video.currentTime = 0;
+        }
+      }
     });
+
+    stopTimer();
+    if (!slides[current].querySelector('video')) startTimer();
   }
 
   function advance(dir) {
@@ -30,14 +44,25 @@
       if (slide.classList.contains('is-prev')) advance(-1);
       else if (slide.classList.contains('is-next')) advance(1);
     });
+
+    const video = slide.querySelector('video');
+    if (video) {
+      video.addEventListener('ended', () => {
+        if (!slide.classList.contains('is-active')) return;
+        setTimeout(() => {
+          if (slide.classList.contains('is-active')) advance(1);
+        }, 1000);
+      });
+    }
   });
 
   function startTimer() { timer = setInterval(() => advance(1), 4500); }
   function stopTimer() { clearInterval(timer); }
 
   outer.addEventListener('mouseenter', stopTimer);
-  outer.addEventListener('mouseleave', startTimer);
+  outer.addEventListener('mouseleave', () => {
+    if (!slides[current].querySelector('video')) startTimer();
+  });
 
   update();
-  startTimer();
 }());
